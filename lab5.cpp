277cfdc2f1456a5a5bb372fd0383a1197d01a3df
Popa Dumitru <153455730+Kr1mzy@users.noreply.github.com>
2023-12-14 20:42:18 +0200

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 enum class Gender { Male, Female };
 
@@ -12,3 +13,17 @@ std::string gender;
 
 int main()
 {
+std::vector<Person> persons;
+std::string name;
+int age;
+std:: string genderInput;
+
+int numberOfPeople;
+std::cout <<"enter number of people:";
+std::cin >>numberOfPeople;
+
+std::cout <<"enter name:";
+std::cin >>name;
+
+std::cout <<"enter age:";
+std::cin >>age;
